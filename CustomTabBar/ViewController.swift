a52714cff23c22c33f01d7e85374a717ffc623d0
Felix Titov <titow.feliks@yandex.ru>
2022-08-06 00:26:56 +0300

    Initial Commit

---
+++ CustomTabBar/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CustomTabBar
+//
+//  Created by Felix Titov on 8/6/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
